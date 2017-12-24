ec3fbe256f375c667c7a043714d700a6aa025570
Scott Baker <smbaker@gmail.com>
2017-12-23 21:05:08 -0800

    some checkin from some time ago

--- updatewd.sh
+++ updatewd.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
+rm -f workdisk.img
+cp bootdisks/workdisk.orig workdisk.img
 losetup /dev/loop0 workdisk.img
 mount -t msdos /dev/loop0 /mnt/msdos
 rm -f /mnt/msdos/FIND.EX /mnt/msdos/PRINT.COM /mnt/msdos/GRAFTABL.COM /mnt/msdos/GRAPHICS.COM
 cp flashbio/flashbio.com /mnt/msdos/
-cp flashbio/TESTBRD.EXE /mnt/msdos/
-cp flashbio/TESTSZ.EXE /mnt/msdos/
-cp flashbio/TESTPAR.EXE /mnt/msdos/
+cp flashbio/FLASHUTL.EXE /mnt/msdos/
 umount /mnt/msdos
 losetup -d /dev/loop0
